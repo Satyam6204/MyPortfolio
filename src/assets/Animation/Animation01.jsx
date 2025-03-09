31df695a06dcde0100336b792442728ec1896034
Satyam6204 <satu45025@gmai.com>
2025-03-09 22:30:04 +0530

    commit2nd

---
+++ src/assets/Animation/Animation01.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react"; // Using DotLottieReact only
+
+
+function Animation01() {
+    return (
+        <DotLottieReact
+            src="https://lottie.host/9d1991cf-906e-4d68-aa08-85a381d37562/Zz5W1Xafo0.lottie"
+            loop
+            autoplay
+
+        />
+    );
+}
+
+export default Animation01;
---
+++ src/assets/pages/Projects.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../Components/Footer'
+
+function Projects() {
+    return (
+        <>
+            <div className="flex justify-center ">
+                <img className="w-100 xl:w-200 md:w-150" src="https://static.vecteezy.com/system/resources/previews/000/192/089/original/website-under-construction-illustration-vector.jpg" alt="img2" />
+            </div>
+
+            <Footer />
+        </>
+    )
+}
+
+export default Projects
